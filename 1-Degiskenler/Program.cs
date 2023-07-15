f20769f574196b839b65f66b3a9820c5227c60f0
KURT <KURT@KURT>
2023-07-15 11:08:28 +0300

    Add project files.

---
+++ 1-Degiskenler/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, World!");
+string name;///null
+int age;//0
+bool isActive;//False
+string nameLastName = "Ramazan Kurt";
+
+char karakter = 'A';
+
+Console.WriteLine(nameLastName);
